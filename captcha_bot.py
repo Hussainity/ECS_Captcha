92880547e8da19b1fc54a1a6f66cbf9673c077fe
Hussain Miyaziwala <hussain_miya@hotmail.com>
2022-04-09 13:46:33 -0400

    added try loop and tested

--- captcha_bot.py
+++ captcha_bot.py
@@ -8,7 +8,7 @@ PATH = "C:/Users/hussa/Documents/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 # TODO: replace with toplist urls
-urls = ["https://www.facebook.com"]
+urls = ["https://www.yahoo.com"]
 
 urls_no_login = []
 
@@ -19,46 +19,68 @@ for url in urls:
     sleep(1) 
 
     # find sign in
-    count = 1 # set this to the number of ways we are looking for the log in button
+    count = 7 # set this to the number of ways we are looking for the log in button
     while (count >= 0):
         try:
-            if (count == 4): x = driver.find_element(By.XPATH, "//*[text()='sign in']"); break
-            if (count == 3): x = driver.find_element(By.XPATH, "//*[text()='log in']"); break
-            if (count == 2): x = driver.find_element(By.XPATH, "//*[text()='Sign In']"); break
-            if (count == 1): x = driver.find_element(By.XPATH, "//*[text()='Log In']"); break
-            if (count == 0): urls_no_login.append(url); break # no log in found
+            # KORI: Follow this pattern to add new lines to find the log in button
+            if (count == 7): driver.find_element(By.XPATH, "//*[text()='sign in']").click(); break
+            if (count == 6): driver.find_element(By.XPATH, "//*[text()='log in']").click(); break
+            if (count == 5): driver.find_element(By.XPATH, "//*[text()='Sign In']").click(); break
+            if (count == 4): driver.find_element(By.XPATH, "//*[text()='Log In']").click(); break
+            if (count == 3): driver.find_element(By.XPATH, "//*[text()='Sign in']").click(); break
+            if (count == 2): x = driver.find_element(By.XPATH, "//*[text()='Log in']").click(); break
+            if (count == 1):
+                driver.get(url + "/login")
+                break
+            if (count == 0):
+                 urls_no_login.append(url); break # no log in found
                 
         except Exception as e:
-            count += 1
+            count -= 1
 
-    x.click()
     # At this point, bot should be at login page... now look for username box
+    sleep(1)
 
-    break
-    count = 1 # set this to the number of ways we are looking for the log in button
+    count = 5 # set this to the number of ways we are looking for the log in button
     while (count >= 0):
         try:
-            if (count == 1):
-                print()
 
+            x = None
+
+            # KORI: Follow this pattern to add new lines to find the user name box
+            if (count == 4): x = driver.find_element(By.ID, "user")
+            if (count == 3): x = driver.find_element(By.ID, "login-username")
+            if (count == 2): x = driver.find_element(By.ID, "username")
+            if (count == 1): x = driver.find_element(By.ID, "email")
             if (count == 0):
-                # report that log in button not found, save it to log
-                urls_no_login.append(url)
+                print("no username found in login page")
                 break
 
+            x.send_keys("ece8803@hotmail.com")
+            break
+
         except Exception as e:
-            count += 1
+            count -= 1
 
 
-    # x = driver.find_element(By.XPATH, "//*[text()='Sign in']")
-    x = driver.find_element(By.XPATH, "//*[text()='Log In']")
-    x.click()
-    sleep(2) 
+    print(urls_no_login)
+    sleep(2)
+    driver.close()
+    break
+
+
+
+
+
+### WORK BOX BELOW - IGNORE ###
+
+'''
+
 
     # find username password input boxes
     # driver.find_element(By.ID, "login-username").send_keys("hotdog@hotdog.com" + Keys.ENTER)
-    # x = driver.find_element(By.ID, "identifierId")
-    x = driver.find_element(By.ID, "email")
+    # 
+    
     x.send_keys("hotdog@hotdog.com")
     x = driver.find_element(By.ID, "pass")
     x.send_keys("SUHSUHSUH1!" + Keys.ENTER)
@@ -81,7 +103,7 @@ for url in urls:
 
 
 
-'''
+
 1. have a list of URLs want to access alexa top 50
 
 2. search the page for a sign in link, click on it
